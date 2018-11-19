6aa44f395d5853fc2e03bbca577d04632a57bb68
李汝飞 <lirufei0808@gmail.com>
2018-11-19 23:24:07 +0800

    添加开发版本

---
+++ generators/model/index.js
@@ -0,0 +1,17 @@
+let artifactInfo = {
+  type: 'model',
+  rootDir: 'src',
+  outDir: '/home/lg/workspace/nodejs/todo/src/models',
+  properties:
+    {
+      id: {type: '\'number\'', id: true, tsType: 'number'},
+      name: {type: '\'string\'', tsType: 'string'},
+    },
+  modelDir: '/home/lg/workspace/nodejs/todo/src/models',
+  name: 'test',
+  relPath: 'src/models',
+  className: 'Test',
+  modelBaseClass: 'Entity',
+  outFile: 'test.model.ts',
+  isModelBaseBuiltin: true,
+};
