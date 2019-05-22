191cda68a4f3547e1b9e9916620110452f3b389e
李汝飞 <lirufei0808@gmail.com>
2019-05-22 14:52:49 +0800

    fix no database name bug

---
+++ config.js
@@ -0,0 +1,37 @@
+//<Config at here>
+const api_server_port = process.env['API_SERVER_PORT'] || '3080'; //this api-server port
+const tapdata_port = process.env['TAPDATA_PORT'] || '3030';  //moa server port
+const tapdata_host = process.env['TAPDATA_HOST'] || '127.0.0.1';  //moa server ip
+const tapdata_origin = process.env['TAPDATA_ORIGIN'] || `http://${tapdata_host}:${tapdata_port}`;
+const process_id = process.env['API_SERVER_ID'] || 'd2f1cc40-552a-11e9-8ff4-059b83989412';
+//</Config at here>
+
+
+const config = {
+	'version': 'v1.0.0-0-gd48b0c9',
+	'intervalsDesc': 'The intervals to check the api definition, unit is milliseconds.',
+	'intervals': 5000,
+	'host': '0.0.0.0',
+	'port': api_server_port,
+	'tapDataServer': {
+		'url': `${tapdata_origin}/api/apiModules`,
+		'tokenUrl': `${tapdata_origin}/api/users/generatetoken`, // url to get token by accessCode
+		'reportUrl': `${tapdata_origin}/api/Workers/upsertWithWhere`,
+		'logUrl': `${tapdata_origin}/api/Logs`,
+		'apiCallsUrl': `${tapdata_origin}/api/ApiCalls`,
+		'accessCode': 'bd16c77a-2111-499c-b2ae-a35c587ea83a',
+	},
+
+	'reportIntervals': 5000, // milliseconds
+	'reportData': {
+		'worker_type': 'api-server',
+		'process_id': process_id
+	},
+
+	'cacheDir': 'cache',
+	'logDir': 'logs',
+
+	'jwtSecretKey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+};
+
+module.exports = config;
--- datasource.js
+++ datasource.js
@@ -9,6 +9,7 @@ const path = require('path');
 const appConfig = require('./config');
 const getToken = require('./tapdata').getToken;
 const MongoClient = require('mongodb').MongoClient;
+const parse = require('mongodb-core').parseConnectionString;
 const parseSchema = require('mongodb-schema');
 
 
@@ -46,11 +47,8 @@ testConnection = function(connection){
 
 			//const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true";
 			const uri = connection.database_uri;
-			const client = new MongoClient(uri, {
-				useNewUrlParser: true
-			});
 			const validate_details = [];
-			client.connect(err => {
+			parse(uri, (err, uriObj) => {
 
 				validate_details.push({
 					"stage_code" : "validate-3000",
@@ -69,68 +67,113 @@ testConnection = function(connection){
 						response_body: {
 							validate_details: validate_details
 						}
-					}, function(err, data){
-						client.close();
-					});
+					}, function(err, data){});
+				}
+
+				validate_details.push({
+					"stage_code" : "validate-3100",
+					"show_msg" : "Checking the connection database name is available.",
+					"status" : uriObj['defaultDatabase'] ? "passed" : "invalid",
+					"sort" : 1,
+					"error_code" : null,
+					"fail_message" : uriObj['defaultDatabase'] ? null : "No database name",
+					"required" : true
+				});
+
+				if( !uriObj['defaultDatabase'] ){
+					updateConnection(connection.id, {
+						status: 'invalid',
+						response_body: {
+							validate_details: validate_details
+						}
+					}, function(err, data){});
 				} else {
-					const db = client.db();
-					log.info('connected mongodb, database name is ' + db.databaseName);
+					const client = new MongoClient(uri, {
+						useNewUrlParser: true
+					});
+					client.connect(err => {
 
-					db.collections(function(err, collections){
-
-						let pending = 0;
-						const schema = {
-							tables: []
-						},
-						errors = [],
-						finish = function(){
-							pending--;
-							if(pending === 0) {
-
-								validate_details.push({
-									"stage_code" : "validate-3200",
-									"show_msg" : "Trying to load schema.",
-									"status" : errors.length === 0 ? "passed" : "invalid",
-									"sort" : 2,
-									"error_code" : null,
-									"fail_message" : errors.length > 0 ? errors.join("\n") : null,
-									"required" : true
-								});
-
-								updateConnection(connection.id, {
-									status: 'ready',
-									schema: schema,
-									response_body: {
-										validate_details: validate_details
-									}
-								}, function(err, result){
-									if(err){
-										log.error('test mongodb connection fail\n', err);
-									} else {
-										log.info('test mongodb connection done.');
-									}
-									client.close();
-								});
-							}
-						};
-						collections.forEach((collection) => {
-							pending++;
-							getCollectionSchema(collection, function(err, collectionSchema){
-								if( err){
-									log.error('get collection schema fail\n', err);
-									errors.push(err);
-								} else {
-									schema.tables.push(collectionSchema);
-								}
-								finish();
-							});
-
+						validate_details.push({
+							"stage_code" : "validate-3100",
+							"show_msg" : "Checking the connection is available.",
+							"status" : err ? "invalid" : "passed",
+							"sort" : 1,
+							"error_code" : null,
+							"fail_message" : err ? err.toString() : null,
+							"required" : true
 						});
 
+						if(err){
+							log.error('connect to mongodb error\n', err);
+							updateConnection(connection.id, {
+								status: 'invalid',
+								response_body: {
+									validate_details: validate_details
+								}
+							}, function(err, data){
+								client.close();
+							});
+						} else {
+							const databaseName = uriObj['defaultDatabase'] || 'test';
+							const db = client.db(databaseName);
+							log.info('connected mongodb, database name is ' + db.databaseName);
+
+							db.collections(function(err, collections){
+
+								let pending = 0;
+								const schema = {
+										tables: []
+									},
+									errors = [],
+									finish = function(){
+										pending--;
+										if(pending === 0) {
+
+											validate_details.push({
+												"stage_code" : "validate-3200",
+												"show_msg" : "Trying to load schema.",
+												"status" : errors.length === 0 ? "passed" : "invalid",
+												"sort" : 2,
+												"error_code" : null,
+												"fail_message" : errors.length > 0 ? errors.join("\n") : null,
+												"required" : true
+											});
+
+											updateConnection(connection.id, {
+												status: 'ready',
+												schema: schema,
+												response_body: {
+													validate_details: validate_details
+												}
+											}, function(err, result){
+												if(err){
+													log.error('test mongodb connection fail\n', err);
+												} else {
+													log.info('test mongodb connection done.');
+												}
+												client.close();
+											});
+										}
+									};
+								collections.forEach((collection) => {
+									pending++;
+									getCollectionSchema(collection, function(err, collectionSchema){
+										if( err){
+											log.error('get collection schema fail\n', err);
+											errors.push(err);
+										} else {
+											schema.tables.push(collectionSchema);
+										}
+										finish();
+									});
+
+								});
+
+							});
+						}
 					});
 				}
 			});
-
 		}
 	} catch (e) {
 		log.error('test connection fail\n', e);
